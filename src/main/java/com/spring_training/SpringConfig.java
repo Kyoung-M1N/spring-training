2ac1a8f7ebe797ef2e8f4fcadcc1404a0a1ec34c
kymin <park48664866@gmail.com>
2023-12-11 12:11:20 +0900

    feat: 스프링빈 등록

---
+++ src/main/java/com/spring_training/SpringConfig.java
@@ -0,0 +1,35 @@
+package com.spring_training;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.spring_training.service.MemberService;
+import com.spring_training.repository.MemberRepository;
+import com.spring_training.repository.MemoryMemberRepository;
+
+/*
+ * 스프링빈 등록 기능을 하는 클래스를 생성하고 @Configuration으로 스프링빈을 등록하는 클래스임을 선언
+ * @Configuration과 @Bean을 통해 스프링빈으로 등록할 클래스의 생성자를 이용하여 스프링빈을 등록
+ * 현재 코드에서 MemberRepository에 대한 구현클래스가 변경될 예정이므로 자바코드를 통해 스프링빈을 등록
+ */
+@Configuration  // 자바 코드로 직접 스프링 빈 등록
+public class SpringConfig {
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
+
+/*
+ * DI에는 필드 주입, setter 주입, 생성자 주입 이렇게 3가지 방법이 있다.
+ * 의존관계가 실행중에 동적으로 변하는 경우는 거의 없으므로 생성자 주입을 권장
+ * 실무에서는 주로 정형화된 컨트롤러, 서비스, 리포지토리 같은 코드는 컴포넌트 스캔을 사용
+ * 정형화 되지 않거나, 상황에 따라 구현 클래스를 변경해야 하면 자바코드를 통해 스프링 빈으로 등록
+ * @Autowired를 통한 DI는 helloController, memberService등과 같이 스프링이 관리하는 객체에서만 동작
+ * 스프링 빈으로 등록하지 않고 내가 직접 생성한 객체에서는 동작하지 않는다.
+ */
---
+++ src/main/java/com/spring_training/controller/MemberController.java
@@ -0,0 +1,36 @@
+package com.spring_training.controller;
+
+import com.spring_training.service.MemberService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+
+// controller가 외부 요청을 받고, 응답을 리턴
+// service에서 비즈니스 로직 구현
+// repository에서 데이터를 저장 및 관리
+
+/*
+ * controller가 memberService에 의존하고 service가 repository에 의존하기
+ * 때문에 어노테이션을 통해 스프링 빈에 등록 
+ * 기본적으로 같은 패키지(또는 하위)에 포함된 클래스만 컴포넌트 스캔이 된다.
+ * component 스캔방법으로 스프링 빈 등록 @Component로도 가능하며 @Service, @Repository,
+ * @Controller는 @Component를 포함하고 있다.
+ * 기본적으로 스프링빈에 등록된 객체는 싱글톤으로 등록되기 때문에 같은 인스턴스를 사용한다.
+ */
+@Controller //component 스캔 방법으로 스프링빈 등록
+public class MemberController {
+    private final MemberService memberService;
+
+    // controller가 MemberService에 의존하기 때문에 스프링 빈에 등록된 객체를 
+    // 스프링 컨테이너에서 찾아 넣어준다.(의존성 주입)
+    @Autowired  
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
+/*
+ * controller는 주로 service와 repository등에서 데이터를 가져와 view에 전송하는 역할을 한다.
+ * 따라서 controller는 service와 repository등에 의존한다.
+ * @controller를 통해 스프링은 동작시에 스프링 컨테이너에 controller객체를 스프링 빈으로 등록된다.
+ * 스프링빈으로 등록된 객체는 스프링이 관리한다.
+ */
