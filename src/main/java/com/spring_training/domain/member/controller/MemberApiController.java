0442088a357e38ba498575d3525448ec5e58bd64
kymin <park48664866@gmail.com>
2024-01-05 16:29:25 +0900

    feat: spring security 설정 추가

--- src/main/java/com/spring_training/domain/member/controller/MemberApiController.java
+++ src/main/java/com/spring_training/domain/member/controller/MemberApiController.java
@@ -1,17 +1,11 @@
 package com.spring_training.domain.member.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
 
 import com.spring_training.domain.member.Member;
 import com.spring_training.domain.member.dto.MemberDto;
 import com.spring_training.domain.member.service.MemberService;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("/api")
---
+++ src/main/java/com/spring_training/global/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.spring_training.global;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())   //csrf사용 안 함
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/**").permitAll());
+                        //localhost:8080의 모든 경로에 대한 접근 허용
+        return http.build();
+    } // SecurityFilterChain을 주어진 기능을 통해 코드로 설정
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
